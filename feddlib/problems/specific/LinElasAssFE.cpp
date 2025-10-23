6ccd075691cbfef679b37c9b2d54cf5325c29581
LeaSassmannshausen <84917921+LeaSassmannshausen@users.noreply.github.com>
2025-10-23 13:59:20 +0200

    Adding pressure boundary conditions (#89)
    * Update in FSI example
    
    * Adding boundary conditions and setting units in fsi test
    
    * Adding multiple things to parameter list and adjusting output
    
    * Udate in Absorbing boundary condition
    
    * Udate in Absorbing boundary condition
    
    * Making a few adjustments
    
    * Update geomertry paramaters
    
    * Specify material model
    
    * Fixing error in Main of FSI
    
    * Removing deep copy notification as it is annoying
    
    * Update again Paramters
    
    * Update inflow function
    
    * Using Flowrate
    
    * Using Flowrate
    
    * Using absolute value of flowrate
    
    * Using absolute value of flowrate
    
    * Using absolute value of flowrate
    
    * Fix small bug
    
    * Adding true parabolic profile
    
    * Adding stuff
    
    * Update on Artery Curved Benchmark
    
    * Update boundary conditions
    
    * Update mesh and surface flags
    
    * Remove some output
    
    * Remove some output
    
    * Changing some parameter locations
    
    * Addinf heart beat
    
    * Adding heart beat to fsi
    
    * Adding heart beat to fsi
    
    * Adding heart beat to fsi
    
    * Removing Outlet and Inlet area from geometry problem
    
    * Removing Outlet and Inlet area from geometry problem
    
    * Removing Outlet and Inlet area from geometry problem
    
    * Update mesh and interface
    
    * Update mesh and interface
    
    * Update Test and Parameters
    
    * Add new geometries and modify rb
    
    * Update some BC in solid Mesh
    
    * Update mesh Bugs
    
    * Update surface normals
    
    * Adding Realistic Artery Test
    
    * Adding some parameters and updating flow direction
    
    * Add mesh conversion
    
    * Update Laplace Solution
    
    * Update Laplace Solution
    
    * Update parameterlists
    
    * Adding block prec to fsi
    
    * Adding block prec to fsi
    
    * Adding block prec to fsi
    
    * Adding block prec to fsi
    
    * Adding debugging info
    
    * Update block prec in FaCSI
    
    * Adding new fsi simple mesh for tests, adding fluid problem to FaCSI instead of dummy to enable feddlib block prec to be used for FaCSI
    
    * Updating some things n tests
    
    * Multiple updates in fsi problem
    
    * Adding AssFE
    
    * Reduce flow rate
    
    * Test rb flowrate
    
    * Test rb flowrate
    
    * Test rb flowrate
    
    * Test rb flowrate
    
    * Test rb flowrate
    
    * Test for interface in FSI
    
    * Using AssFE
    
    * Adding more precise parameter to linElas
    
    * Switching to Assfe againn
    
    * LinElas
    
    * LinElasAssFE
    
    * Adding comments to FSI
    
    * removing redundant LinElasAssFE and NonLinElasAssFE
    
    * Further cleanup
    
    * Bugfix
    
    * Update one test
    
    * Revert laplace class to original settings
    
    * Revert laplace class to original settings
    
    * Add const conversion. Otherwise I cannot compile it.
    
    * Adding comments in BC Builder and NavierStokes class, correcting meshes of FSI benchmark
    
    ---------
    
    Co-authored-by: Jascha Knepper <jascha.knepper@uni-koeln.de>

--- feddlib/problems/specific/LinElasAssFE.cpp
+++
@@ -1,8 +0,0 @@
-#include "LinElasAssFE_decl.hpp"
-
-#ifdef HAVE_EXPLICIT_INSTANTIATION
-#include "LinElasAssFE_def.hpp"
-namespace FEDD {
-    template class LinElasAssFE<default_sc, default_lo, default_go, default_no>;
-}
-#endif
--- feddlib/problems/specific/NonLinElasAssFE.cpp
+++
@@ -1,7 +0,0 @@
-#include "NonLinElasAssFE_decl.hpp"
-#ifdef HAVE_EXPLICIT_INSTANTIATION
-#include "NonLinElasAssFE_def.hpp"
-namespace FEDD{
-template class NonLinElasAssFE<default_sc, default_lo, default_go, default_no>;
-}
-#endif  // HAVE_EXPLICIT_INSTANTIATION
